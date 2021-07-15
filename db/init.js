c016ebe1a07ef6f7e18d5ce20488f9846f9bcdf9
monto8790 <monto8790@gmail.com>
2021-07-15 17:45:09 +0900

    test

---
+++ db/init.js
@@ -0,0 +1,44 @@
+// const config = require('config');
+const mongoose = require('mongoose');
+
+// const db_config = config.get('db');
+let db_url = '';
+// if (true === db_config.auth) {
+//   db_url = db_config.scheme + db_config.id + ':' + db_config.pw + '@' + db_config.host + ':' + db_config.port + '/' + db_config.name;
+//   console.log('connecting to the DB with authorization...');
+// } else {
+//   db_url = db_config.scheme + db_config.host + ':' + db_config.port + '/' + db_config.name;
+//   console.log('connecting to the DB without authorization...');
+// }
+
+// exports.init_db = async function () {
+//   try {
+//     mongoose.connect(db_url, {
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//       useUnifiedTopology: true
+//     })
+//         .catch(err => console.log(err.message));
+//
+//     // if there's no <cb> resource, then create it
+//     const cb_res = await cb.CB.findOne({ ty: 5 }, { _id: true });
+//     if (null == cb_res) {
+//       const cb_ri = await cb.create_a_cb();
+//
+//       // create the default <acp> resource and put it into acpi of <cb> resource
+//       if (true == await create_default_acp(cb_ri)) {
+//         console.log('successfully created the default <acp> resource and put into acpi of <cb> resource');
+//
+//       }
+//       else {
+//         console.log('error occured during default acp creation for <cb> resource');
+//       }
+//     } else {
+//       console.log('\n<cb> resource already exists with ri: ', cb_res._id);
+//     }
+//   }
+//   catch (err) {
+//     console.error(err);
+//   }
+//
+// }
---
+++ server.js
@@ -0,0 +1,92 @@
+var createError = require('http-errors');
+const express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+// // var indexRouter = require('./routes/index');
+// var getmodel = require('./routes/getmodel');
+// var postmodel = require('./routes/postmodel');
+
+const app = express ();
+var data = [];
+const app = app.listen(3001, () => {
+    console.log("Start Server : localhost:3001");
+})
+app.use(express.json());
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+//ML생성
+app.post('/Models', async (req, res) => {
+    // res.send("connect!");
+    //라우터 붙이기
+    //db 붙이기
+    //
+    // console.log(req.body);
+    data.push(req.body);
+    console.log(data)
+    res.json(data);
+})
+
+app.put('/Models/:model_id', async (req, res) => {
+
+    let model_id = req.params["model_id"];
+    let arr = data.map(data => {
+
+        if(data.id != model_id)
+            // data.name = name
+            data.id = model_id;
+            console.log(model_id);
+
+        return {
+            id: data.id,
+
+        }
+    })
+    // if(req.params["model_id"] == id){
+    //     console.log("ok");
+    // }
+    //db에 쿼리 요청을 통해 해당 id에 맞는 데이터를 불러와 수정을 함 or 해당 디렉토리의 수정 과정을 거침
+    //수정 model name
+    // platform name 수정
+    // verison 수정
+    // model file수정?
+    res.json(data);
+});
+
+
+app.delete('/Models/:model_id', async (req, res) => {
+    // const user_id = req.query.user_id
+    let model_id = req.params["model_id"];
+    //filter라는 함수는 자바스크립트에서 배열 함수이다. 필터링을 할때 많이 사용된다 필터링한 데이터를 새로운 배열로 반환한다.
+    const user = data.filter(data => data.id != model_id );
+    console.log(user);
+    data = user;
+    res.send(200);
+})
+
+app.get('/Models', async (req, res) => {
+    // res.send("connect!");
+    res.send(data);
+
+})
+
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+//
+// // error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
